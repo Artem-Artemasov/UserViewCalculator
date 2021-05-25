ec79c86ee3651390389340630ed36c1654d9124a
Artem-Artemasov <artem.artemasov@ukad-group.com>
2021-05-25 13:03:44 +0300

    Add project files.

---
+++ WrapperOverCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WrapperOverCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new UserViewCalculator().Start();
+        }
+    }
+}
---
+++ WrapperOverCalculator/UserCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringCalculator;
+
+namespace WrapperOverCalculator
+{
+    class UserViewCalculator
+    {
+        private readonly Calculator calculator = new Calculator();
+        
+        public void Start()
+        {
+            int countAction = 0;
+            int result = 0;
+            while (true)
+            {
+                result = 0;
+                if (countAction == 0)
+                {
+                    Console.WriteLine("Enter comma separated numbers (enter to exit):");
+                }
+                else
+                {
+                    Console.WriteLine("You can enter other numbers (enter to exit)?");
+                }
+
+                string inputNumbers = Console.ReadLine();
+                if (inputNumbers.Length == 0) return;
+
+                result = calculator.Add(inputNumbers);
+                Console.WriteLine($"Result is {result}");
+            }
+        }
+    }
+}
